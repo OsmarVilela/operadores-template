8372e2d7edb3f8703ae18087991cbb3beb6744f7
OsmarVilela <osmarhenrique77@gmail.com>
2023-02-01 22:56:02 -0300

    exercicio feitos na aula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,62 @@
 //CODIGO AQUI
+// let saldo 
+
+// saldo = 15
+// saldo = saldo + 22.4
+// console.log("Valor do saldo após a primeira operaçao", saldo)
+
+// saldo = saldo - 12.34
+// console.log(`Saldo após a segunda operação ${saldo}`)
+
+// saldo = saldo / 3
+// console.log("saldo dividido", saldo)
+
+// saldo = saldo * 3.42
+// console.log("Meu saldo após investimento", saldo)
+
+// // saldo assumira o resto da divisão do dinheiro por 3
+// saldo = saldo % 3 
+// console.log("Agora eu tenho", saldo)
+
+// Pratica guiada 2
+// resultado1 = 5 === 5 
+// console.log(resultado1)
+
+// resultado2 = 5 !== 5
+// console.log(resultado2)
+
+// resultado3 = 5 !== "5"
+// console.log(resultado3)
+
+// resultado4 = "5" === "cinco"
+// console.log(resultado4)
+
+// resultado5 = typeof 5 === typeof 20
+// console.log(resultado5)
+
+// resultado6 = typeof "5" === typeof "cinco"
+// console.log(resultado6)
+// // final exercicio
+
+// PRATICA GUIADA 3
+
+/*const primeiroNumero = 15
+const segundoNumero = 24
+
+//comparação 1
+const compara1 = primeiroNumero === segundoNumero
+console.log("O primeiro número é igual ao segundo?", compara1)
+
+// comparação 2
+const compara2 = primeiroNumero <= segundoNumero
+console.log("O primeiro número é menor ou igual ao segundo?",compara2)
+
+// comparação 3
+const compara3 = primeiroNumero > segundoNumero
+console.log("O primeiro número é maior que o segundo?", compara3)
+
+// comparação 4
+const compara4 = primeiroNumero < segundoNumero
+console.log("O primeiro número é menor que o segundo?", compara4)
+*/
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,39 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
+/*const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
 
+comparIdade = minhaIdade > idadeAmiga
+console.log("Sua idade é maior que a do seu melhor amigo?", comparIdade)
+
+const diferençaIdade = minhaIdade - idadeAmiga
+console.log("A diferença de idade é", diferençaIdade, "Anos")
+*/
+
+
+
 // EXERCICIO 02
+/* 
+1) Não sou maior de idade e quero passear de carro
+2) Sou maior de idade e vou para a praia, e não quero passear de carro
+3) Vou para a praia ou quero passear de carro
+4) Posso tomar uma cervejinha e não vou passear de carro*/
+
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+const pergunta1 = !b && d
+const pergunta2 = b && a && !d
+const pergunta3 = a || d
+const pergunta4 = c && !d
+
+console.log(`Não sou maior de idade e quero passear de carro: ${pergunta1} 
+
+Sou maior de idade e vou para a praia, e não quero passear de carro: ${pergunta2}
+
+Vou para a praia ou quero passear de carro: ${pergunta3}
+
+Posso tomar uma cervejinha e não vou passear de carro: ${pergunta4}`)
+
+// final exercicio
